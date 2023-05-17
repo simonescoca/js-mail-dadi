cafd664113476e7a1c9903ef7371ab8c0a3746d4
simonescoca <simone.scoca@icloud.com>
2023-05-17 13:09:46 +0200

    added deliveries in js files

---
+++ Dadi/dadi.js
@@ -0,0 +1,6 @@
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+// Teniamo presente che per il gioco dei dadi non serviranno gli array, ma per l'esercizio delle mail....
---
+++ Mail/mail.js
@@ -0,0 +1,3 @@
+// Mail
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+// Tenete presente che non è permesso utilizzare funzioni built-in degli array come Array.includes() e Array.indexOf(), pena l'annullamento dell'esercizio.
