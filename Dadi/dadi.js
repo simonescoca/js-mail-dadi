cd37e97077e8b262cb638b905176b0b55099c5e1
simonescoca <simone.scoca@icloud.com>
2023-05-17 14:44:51 +0200

    dice game in console.log()

--- Dadi/dadi.js
+++ Dadi/dadi.js
@@ -4,3 +4,14 @@
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 // Teniamo presente che per il gioco dei dadi non serviranno gli array, ma per l'esercizio delle mail....
+
+let numeroDadoGiocatore = parseInt( (Math.random() * 6) + 1)
+let numeroDadoComputer = parseInt( (Math.random() * 6) + 1)
+
+if (numeroDadoComputer > numeroDadoGiocatore) {
+    console.log ("Il Giocatore tira il dado e ottiene un: " + numeroDadoGiocatore + ". Il computer tira il dado e ottiene un: " + numeroDadoComputer + ", vince il Computer!")
+} else if (numeroDadoGiocatore > numeroDadoComputer) {
+    console.log ("Il Computer tira il dado e ottiene un: " + numeroDadoComputer + ". Il giocatore tira il dado e ottiene un: " + numeroDadoGiocatore + ", vince il Giocatore!")
+} else {
+    console.log ("Il Giocatore tira il dado e ottiene un: " + numeroDadoGiocatore + ". Il computer tira il dado e ottiene un: " + numeroDadoComputer + ". Stesso risultato, si rigioca!")
+}
